36769c344db6b79185d1eddcf04362fc49a791a6
arifdwis <arifdwi286@gmail.com>
2026-07-27 16:24:04 +0800

    fix: remove unparsed @routes directive from app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,7 +10,6 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
-    @routes
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @inertiaHead
 </head>
